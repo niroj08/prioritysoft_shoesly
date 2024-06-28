9eea90092bda6455a1de5ab12c74978f5fa72cd2
Niroj Shr <shr.niroj08@gmail.com>
2024-06-28 22:21:46 +0545

    Code comment for firestore db file

--- lib/repository/firestore_database.dart
+++ lib/repository/firestore_database.dart
@@ -4,11 +4,13 @@ class FirestoreDatabaseServie {
   FirebaseFirestore db = FirebaseFirestore.instance;
   QuerySnapshot<Map<String, dynamic>>? collection;
 
+//get all the collection named shoesly from the firestore
   Future<bool> getCollectionData() async {
     collection = await db.collection("shoesly").get();
     return collection != null;
   }
 
+//segregate brand document from the retrieved collection
   QueryDocumentSnapshot<Map<String, dynamic>>? getBrands() {
     QueryDocumentSnapshot<Map<String, dynamic>>? brands;
     if (collection != null) {
@@ -17,6 +19,7 @@ class FirestoreDatabaseServie {
     return brands;
   }
 
+//segregate product document from the retrieved collection
   QueryDocumentSnapshot<Map<String, dynamic>>? getProducts() {
     QueryDocumentSnapshot<Map<String, dynamic>>? products;
     if (collection != null) {
